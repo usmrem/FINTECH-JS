cf2539f1d14a80e9b34240368d53ee3e1719dca7
emirow <emirow@users.noreply.github.com>
2026-04-14 19:01:02 +0500

    changes

--- mysite/mysite.js
+++ mysite/mysite.js
@@ -1,8 +1,21 @@
-    const dots = document.getElementById("dots");
-    const frames = ["", ".", "..", "..."];
-    let i = 0;
+const dots = document.getElementById("dots");
+const frames = ["", ".", "..", "..."];
+let i = 0;
 
-    setInterval(() => {
-      dots.textContent = frames[i];
-      i = (i + 1) % frames.length;
-    }, 400);
+setInterval(() => {
+  dots.textContent = frames[i];
+  i = (i + 1) % frames.length;
+}, 400);
+
+// disable copy
+document.addEventListener("copy", e => e.preventDefault());
+
+// disable right click
+document.addEventListener("contextmenu", e => e.preventDefault());
+
+// disable Ctrl+C
+document.addEventListener("keydown", e => {
+  if (e.ctrlKey && e.key.toLowerCase() === "c") {
+    e.preventDefault();
+  }
+});
