8d60a2d565fc300127e140f86e586c895c843e8c
emirow <emirow@users.noreply.github.com>
2026-06-14 12:25:19 +0500

    Convert react-app from submodule to regular folder

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Alert } from "./Alert";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Alert
+        type="information"
+        heading="Success "
+        closable
+        onClose={() => console.log("closed")}
+      >
+        Everything is really good!
+        </Alert>
+
+      <Alert type="warning" heading="Error " closable>
+        Something went wrong!
+      </Alert>
+
     </div>
   );
 }
